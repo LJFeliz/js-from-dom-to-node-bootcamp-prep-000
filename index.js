97c531c11cfd02fccfbf8c252d891549a0c0d6d1
Luis J Feliz <lfjo.mobil@gmail.com>
2018-07-24 17:47:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 // write your code below!
-console.log ("Hello, World!")
-
-
-if (5 > 2) {
-  console.log("5 is greater than 2")
+var dog = "cuddly"
+ 
+if (dog === "hungry") {
+  console.log("Refilling food bowl.")
+ 
+} else if (dog === "thirsty") {
+  console.log("Refilling water bowl.")
+ 
+} else if (dog === "playful") {
+  console.log("Playing tug-of-war.")
+ 
+} else if (dog === "cuddly") {
+  console.log("Snuggling.")
+ 
+} else { 
+  console.log("Reading newspaper.")
 }
-
-
-if (2 > 5) {
-    console.log("2 is greater than 5")
-}
-
-if (false) {
-   console.log("This will never get printed because the above statement evaluates to false.")
-} else {
-   console.log("This will get printed!")
-}
-
-if (6 + 3 === 9) {
-  console.log("Giraffes have no vocal cords.")
-}
+ 
+This will pri
