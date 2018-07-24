79b955c9b1bc420c5d16cc8cc9c4e95dad081322
Luis J Feliz <lfjo.mobil@gmail.com>
2018-07-24 17:47:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 // write your code below!
 var dog = "cuddly"
- 
+
 if (dog === "hungry") {
   console.log("Refilling food bowl.")
- 
+
 } else if (dog === "thirsty") {
   console.log("Refilling water bowl.")
- 
+
 } else if (dog === "playful") {
   console.log("Playing tug-of-war.")
- 
+
 } else if (dog === "cuddly") {
   console.log("Snuggling.")
- 
-} else { 
+
+} else {
   console.log("Reading newspaper.")
 }
- 
-This will pri
+
