24d7976778f800be0b84f1402bfbc50bb0ad7694
Luis J Feliz <lfjo.mobil@gmail.com>
2018-07-24 17:40:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,9 @@ if (5 > 2) {
 if (2 > 5) {
     console.log("2 is greater than 5")
 }
+
+if (false) {
+   console.log("This will never get printed because the above statement evaluates to false.")
+} else {
+   console.log("This will get printed!")
+}
